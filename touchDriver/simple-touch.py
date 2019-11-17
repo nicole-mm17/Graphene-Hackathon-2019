e064cda7b48735c319a4d5e5a8f5d13415736d53
stanleyycheung <stanleyycheung@gmail.com>
2019-11-17 13:06:16 +0000

    final update

--- touchDriver/simple-touch.py
+++ touchDriver/simple-touch.py
@@ -5,6 +5,12 @@ import MPR121
 import serial
 import copy
 import vlc
+import pygame
+
+file='/home/pi/Documents/grapheneHackathon/touchDriver/mp3/example.mp3'
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load(file)
 
 try:
   sensor = MPR121.begin()
@@ -23,9 +29,7 @@ sensor.set_touch_threshold(touch_threshold)
 sensor.set_release_threshold(release_threshold)
 
 #serial information
-ser = serial.Serial('/dev/ttyACM0', 9600)
-
-p = vlc.MediaPlayer('/home/pi/Documents/grapheneHackathon/touchDriver/mp3/Car Horn Honk 1-SoundBible.com-248048021.wav')
+# ser = serial.Serial('/dev/ttyACM0', 9600)
 
 # loop
 states = [0] * 5
@@ -38,39 +42,18 @@ while running:
       for i in range(0, 5):
         if sensor.is_new_touch(i):
           states[i] = 1
-          # print ("electrode {0} was just touched".format(i))
         elif sensor.is_new_release(i):
           states[i] = 0
-          '''
-          for j in previous:
-            if i == 0:
-              output = b'%d' %(i+10)
-              #ser.write(output)
-            elif j == i:
-              output = b'%d' %(j+10)
-              ser.write(output)
-          '''
-          # print ("electrode {0} was just released".format(i))
       print(states)
-      #count = 0
-      #for i in range(1,len(states)):
-        #if states[0] and states[i]:
-          #ser.write(str(i))
-          #print("finger {0}".format(i))
-      
-      
-  #    print("prev: {}".format(previous))
       for i in range(1, len(states)):
         if states[0] and states[i]:
           if states[i] != previous[i] or states[0] != previous[0]:
-            #output = b'%d' %i
-            ser.write(str(i))
-            #count += 1
+            # ser.write(str(i))
             print("finger {0}".format(i), states)
-            if i == 2 or i == 0:
-              if states[2] == 1:
+            if i == 4 or i == 0:
+              if states[4] == 1:
                 print('BOOP')
-                p.play()
+                pygame.mixer.music.play()
       previous=copy.deepcopy(states)
     
     sleep(0.01)
